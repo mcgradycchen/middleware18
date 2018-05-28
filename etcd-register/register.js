b43279f29bc37bebf2c29934d4c1f1750d422072
Lin Chen <linchen@nj-mcgradychen-centos75.localdomain>
2018-05-28 12:46:49 +0800

    register and discovery service

--- etcd-register/register.js
+++ etcd-register/register.js
@@ -15,7 +15,7 @@ class RegisterService {
         if (config && config.service_type === "provider") {
             this.register_service(config.service_rootpath, config.service_name, config.service_ip, config.service_port);
         }
-        setInterval(this.keep_alive, 80);
+        setInterval(this.keep_alive.bind(this), 80);
     }
 
     register_service(service_rootpath, service_name, service_ip, service_port) {
@@ -26,9 +26,41 @@ class RegisterService {
     }
 
     keep_alive() {
-        return this.lease.keepaliveOnce().then((res) => {
+        var that = this;
+        return that.lease_client.keepaliveOnce().then((res) => {
             logger.info("register_service, keep_alive, lease_id=", JSON.stringify(res));
         });
+    }
+
+    discovery_service(service_rootpath, service_name) {
+        var that = this;
+        var key = service_rootpath + "/" + service_name;
+        var services = [];
+        logger.info("discovery_serivce, filter_key=", key);
+        return that.etcd_client.getAll(key).exec().then(function(res) {
+            logger.info("discovery_service, res=", res);
+            if (res && res.kvs && res.kvs.length > 0) {
+                res.kvs.forEach((item) => {
+                    if(item && item.key) {
+                        var key = item.key.toString('utf8');
+                        logger.debug("discovery_serivce, service_key=", key);
+                        var nPos = key.lastIndexOf("/")
+                        if (nPos >= 0) {
+                            var service = key.substr(nPos + 1);
+                            nPos = service.indexOf(":");
+                            if (nPos > 0) {
+                                services.push({
+                                    "host": service.substr(0, nPos),
+                                    "port": service.substr(nPos + 1)
+                                });
+                            }
+                        }
+                    }
+                });
+            }
+            return services;
+        });
+
     }
 }
 
